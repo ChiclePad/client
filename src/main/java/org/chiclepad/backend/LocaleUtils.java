53f8bc8df6eebc8c1ce34296787e400a1bd71649
Šimon Kocúrek <simon.kocurek@gmail.com>
2017-11-08 01:11:31 +0100

    Add locale utilities (#10)
    Signed-off-by: justmesr <simon.kocurek@gmail.com>

---
+++ src/main/java/org/chiclepad/backend/LocaleUtils.java
@@ -0,0 +1,71 @@
+package org.chiclepad.backend;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Class for working with ChiclePad Locales
+ */
+public class LocaleUtils {
+
+    /**
+     * List of all supported locales
+     */
+    private static final String[] availableLocales = {"sq-AL", "ar-DZ", "ar-BH", "ar-EG", "ar-IQ", "ar-JO", "ar-KW", "ar-LB",
+            "ar-LY", "ar-MA", "ar-OM", "ar-QA", "ar-SA", "ar-SD", "ar-SY", "ar-TN", "ar-AE", "ar-YE", "be-BY", "bg-BG",
+            "ca-ES", "zh-CN", "zh-SG", "zh-HK", "zh-TW", "hr-HR", "cs-CZ", "da-DK", "nl-BE", "nl-NL", "en-AU", "en-CA",
+            "en-IN", "en-IE", "en-MT", "en-NZ", "en-PH", "en-SG", "en-ZA", "en-GB", "en-US", "et-EE", "fi-FI", "fr-BE",
+            "fr-CA", "fr-FR", "fr-LU", "fr-CH", "de-AT", "de-DE", "de-LU", "de-CH", "el-CY", "el-GR", "iw-IL", "hi-IN",
+            "hu-HU", "is-IS", "in-ID", "ga-IE", "it-IT", "it-CH", "ja-JP", "ja-JP-u-ca-japanese", "ja-JP-x-lvariant-JP",
+            "ko-KR", "lv-LV", "lt-LT", "mk-MK", "ms-MY", "mt-MT", "no-NO", "nb-NO", "nn-NO", "no-NO-x-lvariant-NY",
+            "pl-PL", "pt-BR", "pt-PT", "ro-RO", "ru-RU", "sr-BA", "sr-ME", "sr-RS", "sr-Latn-BA", "sr-Latn-ME",
+            "sr-Latn-RS", "sk-SK", "sl-SI", "es-AR", "es-BO", "es-CL", "es-CO", "es-CR", "es-DO", "es-EC", "es-SV",
+            "es-GT", "es-HN", "es-MX", "es-NI", "es-PA", "es-PY", "es-PE", "es-PR", "es-ES", "es-US", "es-UY", "es-VE",
+            "sv-SE", "th-TH", "th-TH-u-ca-buddhist", "th-TH-u-ca-buddhist-nu-thai", "th-TH-x-lvariant-TH", "tr-TR",
+            "uk-UA", "vi-VN"};
+
+
+    /**
+     * @return Locales supported by ChiclePad
+     */
+    public static List<Locale> getAllLocals() {
+        return Arrays.stream(availableLocales)
+                .map(Locale::forLanguageTag)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @return LocaleObject -> "English (Canada)"
+     */
+    public static String localeName(Locale locale) {
+        return locale.getDisplayName();
+    }
+
+    /**
+     * @return "en-CA" -> "English (Canada)"
+     */
+    public static String localeName(String code) {
+        return localeName(Locale.forLanguageTag(code));
+    }
+
+    /**
+     * @return "en-CA" -> LocaleObject
+     */
+    public static Locale localeFromCode(String code) {
+        return Locale.forLanguageTag(code);
+    }
+
+    /**
+     * @return LocaleObject -> "en-CA"
+     */
+    public static Optional<String> codeFromLocale(Locale locale) {
+        return getAllLocals().stream()
+                .map(Locale::toLanguageTag)
+                .filter(code -> code.equals(locale.toLanguageTag()))
+                .findFirst();
+    }
+
+}
