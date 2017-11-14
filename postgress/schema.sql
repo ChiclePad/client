8461778835f739e9f81027ccdf8e121e9cca1365
Šimon Kocúrek <simon.kocurek@gmail.com>
2017-11-14 09:24:53 +0100

    Add table for deleted entries
    Signed-off-by: justmesr <simon.kocurek@gmail.com>

--- postgress/schema.sql
+++ postgress/schema.sql
@@ -1,9 +1,9 @@
 -- User authentification data
 CREATE TABLE chiclepad_user (
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(100) NOT NULL, -- unique will not do, as emails don't depend on casing
-  password VARCHAR(60) NOT NULL CHECK (password != ''),
-  salt CHAR(29) NOT NULL
+  id       SERIAL PRIMARY KEY,
+  email    VARCHAR(150) NOT NULL, -- unique will not do, as emails don't depend on casing
+  password VARCHAR(60)  NOT NULL CHECK (password != ''),
+  salt     CHAR(29)     NOT NULL
 );
 CREATE UNIQUE INDEX email_id on chiclepad_user (lower(email));
 
@@ -24,9 +24,9 @@ CREATE TABLE entry (
 
 -- Category for tagging entries
 CREATE TABLE category (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  icon VARCHAR(50) DEFAULT 'fa-circle-o',
+  id    SERIAL PRIMARY KEY,
+  name  VARCHAR(100) NOT NULL,
+  icon  VARCHAR(50) DEFAULT 'fa-circle-o',
   color CHAR(7) DEFAULT '#cccccc'
 );
 
@@ -39,9 +39,9 @@ CREATE TABLE registered_category (
 
 -- Goals that need to be achieved each day
 CREATE TABLE goal (
-  id SERIAL PRIMARY KEY,
-  entry_id INT REFERENCES entry ON DELETE CASCADE,
-  description TEXT NOT NULL
+  id          SERIAL PRIMARY KEY,
+  entry_id    INT REFERENCES entry ON DELETE CASCADE,
+  description VARCHAR(500) NOT NULL
 );
 
 -- Goal finished succesfully for the day ("exercised today")
@@ -62,20 +62,27 @@ CREATE TABLE diary_page (
 
 -- Smart board of postit notes with reminders
 CREATE TABLE note (
-  id SERIAL PRIMARY KEY,
-  entry_id INT REFERENCES entry ON DELETE CASCADE,
-  position_x INT NOT NULL DEFAULT 0,
-  position_y INT NOT NULL DEFAULT 0,
-  content TEXT NOT NULL,
+  id            SERIAL PRIMARY KEY,
+  entry_id      INT REFERENCES entry ON DELETE CASCADE,
+  position_x    INT           NOT NULL DEFAULT 0,
+  position_y    INT           NOT NULL DEFAULT 0,
+  content       VARCHAR(1000) NOT NULL,
   reminder_time TIMESTAMP
 );
 
 -- One line of a To-do list with interval created by deadlines
 CREATE TABLE todo (
-  id SERIAL PRIMARY KEY,
-  entry_id INT REFERENCES entry ON DELETE CASCADE,
-  description VARCHAR(500) NOT NULL,
-  deadline TIMESTAMP NOT NULL,
+  id            SERIAL PRIMARY KEY,
+  entry_id      INT REFERENCES entry ON DELETE CASCADE,
+  description   VARCHAR(240) NOT NULL,
+  deadline      TIMESTAMP    NOT NULL,
   soft_deadline TIMESTAMP CHECK (deadline > soft_deadline),
-  priority INT DEFAULT 0
+  priority      INT DEFAULT 0
+);
+
+-- Table representing entries that the user already completed
+CREATE TABLE deleted_entry (
+  id           SERIAL PRIMARY KEY,
+  entry_id     INT REFERENCES entry ON DELETE CASCADE,
+  deleted_time TIMESTAMP NOT NULL
 );
