ae9eefb731114f0f9a8af4281458b6d68ac4a143
Šimon Kocúrek <simon.kocurek@gmail.com>
2017-11-15 21:32:19 +0100

    Add scene switching
    Signed-off-by: justmesr <simon.kocurek@gmail.com>

--- src/main/java/org/chiclepad/frontend/jfx/ChiclePadApp.java
+++ src/main/java/org/chiclepad/frontend/jfx/ChiclePadApp.java
@@ -6,12 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class ChiclePadApp extends Application {
 
+    private static Stage primaryStage;
+
     public void start(final Stage primaryStage) throws Exception {
-        MainSceneController mainSceneController = new MainSceneController();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
-        fxmlLoader.setController(mainSceneController);
+        ChiclePadApp.primaryStage = primaryStage;
+
+        LoginSceneController loginSceneController = new LoginSceneController();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("loginScene.fxml"));
+        fxmlLoader.setController(loginSceneController);
         Parent parentPane = fxmlLoader.load();
 
         Scene scene = new Scene(parentPane);
@@ -19,6 +25,12 @@ public class ChiclePadApp extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("ChiclePad");
         primaryStage.show();
+    }
+
+    public static void switchScene(FXMLLoader fxmlLoader) throws IOException {
+        Parent parentPane = fxmlLoader.load();
+        Scene scene = new Scene(parentPane);
+        ChiclePadApp.primaryStage.setScene(scene);
     }
 
 }
