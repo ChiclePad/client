2ed9bf88671088fdaec818133a3516773b713b83
Šimon Kocúrek <simon.kocurek@gmail.com>
2017-11-22 22:53:19 +0100

    Add form validitation
    Signed-off-by: justmesr <simon.kocurek@gmail.com>

--- src/main/java/org/chiclepad/frontend/jfx/ChiclePadApp.java
+++ src/main/java/org/chiclepad/frontend/jfx/ChiclePadApp.java
@@ -4,13 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class ChiclePadApp extends Application {
 
     private static Stage primaryStage;
+
+    private static final Logger logger = LoggerFactory.getLogger(ChiclePadApp.class);
+
+    public static final Color PRIMARY_COLOR = Color.valueOf("#8DC44E");
+
+    public static final Color SECONDARY_COLOR = Color.valueOf("#A24936");
 
     public void start(final Stage primaryStage) throws Exception {
         ChiclePadApp.primaryStage = primaryStage;
@@ -24,13 +34,21 @@ public class ChiclePadApp extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("ChiclePad");
+        primaryStage.getIcons().add(new Image(ChiclePadApp.class.getResourceAsStream("../logo.png")));
         primaryStage.show();
     }
 
-    public static void switchScene(FXMLLoader fxmlLoader) throws IOException {
-        Parent parentPane = fxmlLoader.load();
-        Scene scene = new Scene(parentPane);
-        ChiclePadApp.primaryStage.setScene(scene);
+    public static void switchScene(Object controller, String fxmlPath) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(ChiclePadApp.class.getResource(fxmlPath));
+            fxmlLoader.setController(controller);
+
+            Parent parentPane = fxmlLoader.load();
+            Scene scene = new Scene(parentPane);
+            ChiclePadApp.primaryStage.setScene(scene);
+        } catch (IOException e) {
+            logger.error("Failed switching to scene: " + fxmlPath + "\n" + e.getMessage());
+        }
     }
 
 }
--- src/main/java/org/chiclepad/frontend/jfx/LoginSceneController.java
+++ src/main/java/org/chiclepad/frontend/jfx/LoginSceneController.java
@@ -1,11 +1,11 @@
 package org.chiclepad.frontend.jfx;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 
-import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class LoginSceneController {
 
@@ -15,54 +15,60 @@ public class LoginSceneController {
     @FXML
     private JFXPasswordField passwordField;
 
-    public void switchToRegisterScene() {
-        RegisterSceneController registerSceneController = new RegisterSceneController();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("registerScene.fxml"));
-        fxmlLoader.setController(registerSceneController);
+    @FXML
+    private JFXButton loginButton;
 
-        try {
-            ChiclePadApp.switchScene(fxmlLoader);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private boolean emailValid;
+
+    private boolean passwordValid;
+
+    @FXML
+    public void initialize() {
+        String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08" +
+                "\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:" +
+                "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|" +
+                "[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08" +
+                "\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+        Pattern emailPattern = Pattern.compile(emailRegex);
+
+        emailTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            emailValid = emailPattern.matcher(newValue).matches();
+
+            if (!emailValid) {
+                emailTextField.setFocusColor(ChiclePadApp.SECONDARY_COLOR);
+                emailTextField.setUnFocusColor(ChiclePadApp.SECONDARY_COLOR);
+
+            } else {
+                emailTextField.setFocusColor(ChiclePadApp.PRIMARY_COLOR);
+                emailTextField.setUnFocusColor(ChiclePadApp.PRIMARY_COLOR);
+            }
+
+            loginButton.setDisable(!(passwordValid && emailValid));
+        });
+
+        passwordField.textProperty().addListener((observable, oldValue, newValue) -> {
+            passwordValid = !newValue.isEmpty();
+
+            if (!passwordValid) {
+                passwordField.setFocusColor(ChiclePadApp.SECONDARY_COLOR);
+                passwordField.setUnFocusColor(ChiclePadApp.SECONDARY_COLOR);
+
+            } else {
+                passwordField.setFocusColor(ChiclePadApp.PRIMARY_COLOR);
+                passwordField.setUnFocusColor(ChiclePadApp.PRIMARY_COLOR);
+            }
+
+            loginButton.setDisable(!(passwordValid && emailValid));
+        });
     }
 
-    public void validitateLogin() {
-        // TODO validitate
-        if (emailTextField.getText().isEmpty()) {
-            return;
-        }
-
-        if (passwordField.getText().isEmpty()) {
-            return;
-        }
-//        JFXTextField field = new JFXTextField();
-//        field.setLabelFloat(true);
-//        field.setPromptText("Floating prompt");
-//
-//        JFXTextField validationField = new JFXTextField();
-//        validationField.setPromptText("With Validation..");
-//        RequiredFieldValidator validator = new RequiredFieldValidator();
-//        validator.setMessage("Input Required");
-//        validator.setAwsomeIcon(new Icon(AwesomeIcon.WARNING, "2em", ";", "error"));
-//        validationField.getValidators().add(validator);
-//        validationField.focusedProperty().addListener((o, oldVal, newVal) -> {
-//            if (!newVal) validationField.validate();
-//        });
-
-        switchToHomepageScene();
+    public void onRegisterPressed() {
+        ChiclePadApp.switchScene(new RegisterSceneController(), "registerScene.fxml");
     }
 
-    private void switchToHomepageScene() {
-        HomepageSceneController homepageSceneController = new HomepageSceneController();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("homepageScene.fxml"));
-        fxmlLoader.setController(homepageSceneController);
-
-        try {
-            ChiclePadApp.switchScene(fxmlLoader);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void onLoginPressed() {
+        ChiclePadApp.switchScene(new HomepageSceneController(), "homepageScene.fxml");
     }
 
 }
