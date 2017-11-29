20c88eba4712f2bd99b775ce286db559af0b263a
Šimon Kocúrek <simon.kocurek@gmail.com>
2017-11-29 10:23:05 +0100

    Add controller swithcing
    Signed-off-by: justmesr <simon.kocurek@gmail.com>

--- src/main/java/org/chiclepad/frontend/jfx/startup/LoginSceneController.java
+++ src/main/java/org/chiclepad/frontend/jfx/startup/LoginSceneController.java
@@ -8,7 +8,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import org.chiclepad.frontend.jfx.ChiclePadApp;
 import org.chiclepad.frontend.jfx.ChiclePadColor;
-import org.chiclepad.frontend.jfx.HomepageSceneController;
+import org.chiclepad.frontend.jfx.homepage.TodoSceneController;
 
 public class LoginSceneController {
 
@@ -63,7 +63,7 @@ public class LoginSceneController {
         // TODO login user
         boolean loginSuccesfull = true;
         if (loginSuccesfull) {
-            ChiclePadApp.switchScene(new HomepageSceneController(), "homepageScene.fxml");
+            ChiclePadApp.switchScene(new TodoSceneController(), "todoScene.fxml");
         } else {
             ChiclePadApp.showDialog("Login Failed!", "Check if you entered correct email and password.", overlay);
         }
