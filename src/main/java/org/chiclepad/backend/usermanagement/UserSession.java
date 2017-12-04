a0419d70e7d3b2de4b6ffc15720fbd8df22c8fbc
Mária Babčanská <m.babcanska@gmail.com>
2017-12-04 20:01:42 +0100

    User management (#32)

---
+++ src/main/java/org/chiclepad/backend/usermanagement/UserSession.java
@@ -0,0 +1,51 @@
+package org.chiclepad.backend.usermanagement;
+
+import org.chiclepad.backend.entity.ChiclePadUser;
+
+import java.time.LocalDateTime;
+
+public class UserSession {
+   private ChiclePadUser loggedUser;
+   private int userId;
+   private LocalDateTime registrationDate;
+   private LocalDateTime lastLogInDate;
+
+   public UserSession(final ChiclePadUser loggedUser, final int userId) {
+      this.loggedUser = loggedUser;
+      this.userId = userId;
+   }
+
+   public LocalDateTime getRegistrationDate() {
+
+      return registrationDate;
+   }
+
+   public void setRegistrationDate(final LocalDateTime registrationDate) {
+      this.registrationDate = registrationDate;
+   }
+
+   public LocalDateTime getLastLogInDate() {
+      return lastLogInDate;
+   }
+
+   public void setLastLogInDate(final LocalDateTime lastLogInDate) {
+      this.lastLogInDate = lastLogInDate;
+   }
+
+   public ChiclePadUser getLoggedUser() {
+
+      return loggedUser;
+   }
+
+   public void setLoggedUser(final ChiclePadUser loggedUser) {
+      this.loggedUser = loggedUser;
+   }
+
+   public int getUserId() {
+      return userId;
+   }
+
+   public void setUserId(final int userId) {
+      this.userId = userId;
+   }
+}
