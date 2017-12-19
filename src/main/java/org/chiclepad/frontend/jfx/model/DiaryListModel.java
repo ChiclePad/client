0c35d2d7e46aa1af06c94f0e7bcd6fe660b6cf70
Mária Babčanská <m.babcanska@gmail.com>
2017-12-19 10:01:04 +0100

    Diary scene almost completed (#59)

--- src/main/java/org/chiclepad/frontend/jfx/model/DiaryListModel.java
+++ src/main/java/org/chiclepad/frontend/jfx/model/DiaryListModel.java
@@ -2,7 +2,12 @@ package org.chiclepad.frontend.jfx.model;
 
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.effects.JFXDepthManager;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconName;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -10,6 +15,7 @@ import org.chiclepad.backend.Dao.DaoFactory;
 import org.chiclepad.backend.Dao.DiaryPageDao;
 import org.chiclepad.backend.entity.DiaryPage;
 import org.chiclepad.constants.ChiclePadColor;
+import org.chiclepad.frontend.jfx.ChiclePadApp;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +26,7 @@ public class DiaryListModel {
 
     private VBox layout;
 
-    private HBox selectedDiaryPageLine;
+    private VBox selectedDiaryPageLine;
 
     private DiaryPage selectedDiaryPage;
 
@@ -40,19 +46,56 @@ public class DiaryListModel {
     }
 
     private void addDiaryPageToLayout(DiaryPage diaryPage) {
-        HBox diaryPageLine = new HBox();
+        VBox diaryPageLine = new VBox();
+        diaryPageLine.addEventFilter(MouseEvent.MOUSE_PRESSED, this.selectDiaryPage(diaryPageLine, diaryPage));
+
+        // Hbox containing date and button to display textarea
+        HBox headline = new HBox();
+        Label dateLabel = new Label(diaryPage.getRecordedDay().toString());
+        FontAwesomeIcon dropdownIcon = new FontAwesomeIcon();
+        dropdownIcon.setIcon(FontAwesomeIconName.CARET_DOWN);
         diaryPageLine.getStyleClass().add("form");
         diaryPageLine.setPadding(new Insets(15, 15, 15, 15));
         JFXDepthManager.setDepth(diaryPageLine, 1);
 
-        JFXTextArea text = new JFXTextArea(diaryPage.getText());
-        text.getStyleClass().add("small-normal-text");
-        text.setFocusColor(ChiclePadColor.PRIMARY);
-        text.setUnFocusColor(ChiclePadColor.GREY_TEXT);
-        text.setPrefRowCount(2);
-        HBox.setHgrow(text, Priority.ALWAYS);
+        dropdownIcon.setOnMousePressed(event -> {
+            if (diaryPageLine.getChildren().size() == 2) {
+                // Remove textarea
+                diaryPageLine.getChildren().remove(1);
+            } else if (diaryPageLine.getChildren().size() == 1) {
+                // Add textarea
 
-        diaryPageLine.getChildren().add(text);
+                // TextArea
+                JFXTextArea text = new JFXTextArea(diaryPage.getText());
+                text.textProperty().addListener(((observable, oldValue, newValue) -> {
+                    diaryPage.setText(newValue);
+                }));
+
+                text.focusedProperty().addListener(((observable, oldValue, newValue) -> {
+                    if (newValue) {
+                        return;
+                    }
+                    this.diaryPageDao.update(diaryPage);
+                }));
+
+                text.getStyleClass().add("small-normal-text");
+                text.setFocusColor(ChiclePadColor.PRIMARY);
+                text.setUnFocusColor(ChiclePadColor.GREY_TEXT);
+                if (text.getText().length() < 100) {
+                    text.setPrefRowCount(2);
+                } else if (text.getText().length() > 100) {
+                    text.setPrefRowCount(5);
+                }
+
+                HBox.setHgrow(text, Priority.ALWAYS);
+                diaryPageLine.getChildren().add(text);
+            }
+        });
+
+        headline.getChildren().addAll(dateLabel, dropdownIcon);
+        headline.setSpacing(10);
+        diaryPageLine.getChildren().add(headline);
+        setHighlightOnHover(diaryPageLine, diaryPage);
         layout.getChildren().add(diaryPageLine);
     }
 
@@ -77,6 +120,46 @@ public class DiaryListModel {
         layout.getChildren().removeIf(child -> child == selectedDiaryPageLine);
         diaryPages.remove(selectedDiaryPage);
         return selectedDiaryPage;
+    }
+
+    public EventHandler<MouseEvent> selectDiaryPage(VBox selectedDiaryPageLine, DiaryPage selectedDiaryPage) {
+        return event -> {
+            this.deselectPreviousDiaryPage();
+            setSelectedDiaryPage(selectedDiaryPageLine, selectedDiaryPage);
+            JFXDepthManager.setDepth(selectedDiaryPageLine, 2);
+            this.selectedDiaryPageLine.getStyleClass().add("bordered");
+        };
+    }
+
+    private void setSelectedDiaryPage(VBox selectedDiaryPageLine, DiaryPage selectedDiaryPage) {
+        this.selectedDiaryPageLine = selectedDiaryPageLine;
+        this.selectedDiaryPage = selectedDiaryPage;
+    }
+
+    private void setHighlightOnHover(VBox diaryPageLine, DiaryPage diaryPage) {
+        diaryPageLine.setOnMouseEntered(event -> {
+            diaryPageLine.setStyle("-fx-background-color: " + ChiclePadApp.darken(categoryColorOfDiaryPage(diaryPage), 0.95));
+        });
+
+        diaryPageLine.setOnMouseExited(event -> {
+            diaryPageLine.setStyle("-fx-background-color: " + categoryColorOfDiaryPage(diaryPage));
+        });
+    }
+
+    private String categoryColorOfDiaryPage(DiaryPage diaryPage) {
+        if (!diaryPage.getCategories().isEmpty()) {
+            return diaryPage.getCategories().get(0).getColor();
+        } else {
+            return ChiclePadColor.toHex(ChiclePadColor.WHITE);
+        }
+    }
+
+    private void deselectPreviousDiaryPage() {
+        if (selectedDiaryPageLine == null || selectedDiaryPage == null) {
+            return;
+        }
+        selectedDiaryPageLine.getStyleClass().removeIf(cssClass -> cssClass.equals("bordered"));
+        JFXDepthManager.setDepth(selectedDiaryPageLine, 1);
     }
 
 }
