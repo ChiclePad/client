ea64d9f65a3f5479adbaa0716684509764cf8976
Šimon Kocúrek <simon.kocurek@gmail.com>
2017-12-19 09:57:37 +0100

    Fix category picker
    Signed-off-by: justmesr <simon.kocurek@gmail.com>

--- src/main/java/org/chiclepad/frontend/jfx/model/TodoListModel.java
+++ src/main/java/org/chiclepad/frontend/jfx/model/TodoListModel.java
@@ -5,16 +5,19 @@ import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
+import org.chiclepad.backend.Dao.DaoFactory;
 import org.chiclepad.backend.Dao.NoteDao;
+import org.chiclepad.backend.Dao.TodoDao;
 import org.chiclepad.backend.entity.Category;
 import org.chiclepad.backend.entity.Todo;
 import org.chiclepad.constants.ChiclePadColor;
 
 import java.time.LocalDateTime;
-
 import java.util.List;
 
 public class TodoListModel implements ListModel {
+
+    private TodoDao todoDao = DaoFactory.INSTANCE.getTodoDao();
 
     private ObservableList<TodoTreeItem> todoItems;
 
@@ -56,8 +59,20 @@ public class TodoListModel implements ListModel {
         JFXTreeTableColumn<TodoTreeItem, String> priorityColumn = createPriorityColumn();
 
         todoList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue == null) {
+            if (selectedTodo != null) {
+                todoDao.update(selectedTodo);
+            }
 
+            if (newValue == null) {
+                selectedTodoItem = null;
+                selectedTodo = null;
+
+                descriptionField.setText("");
+                deadlinePicker.setValue(null);
+                softDeadlinePicker.setValue(null);
+                prioritySlider.setValue(0);
+
+                return;
             }
 
             selectedTodoItem = newValue.getValue();
@@ -115,14 +130,7 @@ public class TodoListModel implements ListModel {
     }
 
     public Todo deleteSelected() {
-//        for (int i = 0; i < layout.getCurrentItemsCount(); i++) {
-//            layout.getTreeItem(0)
-//            tableView.getItems().clear();
-//        }
-//
-//        layout. ().removeIf(child -> child == selectedNote);
-//        notes.remove(selectedNote);
-//        return selectedNote;
+        todoItems.remove(selectedTodoItem);
         return selectedTodo;
     }
 
